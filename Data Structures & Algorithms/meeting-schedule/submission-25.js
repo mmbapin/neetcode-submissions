a5afef52bd8f96d19b4dbbd255ec3d15dbc1ca5b
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-04-09 10:18:18 +0600

    Add: meeting-schedule - submission-25

---
+++ Data Structures & Algorithms/meeting-schedule/submission-25.js
@@ -0,0 +1,29 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        let n = intervals.length;
+        for(let i = 0; i < n; i++){
+            let A = intervals[i];
+            for(let j = i + 1; j < n; j++){
+                let B = intervals[j];
+                if(Math.min(A.end, B.end) > Math.max(A.start, B.start)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
