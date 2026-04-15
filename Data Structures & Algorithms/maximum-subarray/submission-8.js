08094f854b5accfb9a983e1ed1dea8ebdb1d84f5
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-04-15 12:22:42 +0600

    Add: maximum-subarray - submission-8

---
+++ Data Structures & Algorithms/maximum-subarray/submission-8.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let n = nums.length;
+        let res = nums[0];
+        for(let i = 0; i < n; i++){
+            let count = 0;
+            for(let j = i; j < n; j++){
+                count += nums[j];
+                res = Math.max(res, count);
+            }
+        }
+        return res;
+    }
+}
