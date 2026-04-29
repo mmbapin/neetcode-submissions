a971c32170a7e15c862427404eb4953937f1e74f
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-04-29 12:06:24 +0600

    Add: spiral-matrix - submission-3

---
+++ Data Structures & Algorithms/spiral-matrix/submission-3.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @return {number[]}
+     */
+    spiralOrder(matrix) {
+        let m = matrix.length;
+        let n = matrix[0].length;
+        let res = [];
+
+        const dfs = (row, col, r, c, dr, dc) => {
+            if(row === 0 || col === 0) return;
+
+            for(let i = 0; i < col; i++){
+                r += dr;
+                c += dc;
+                res.push(matrix[r][c]);
+            }
+            dfs(col, row - 1, r, c, dc, -dr);
+        }
+        dfs(m, n, 0, -1, 0, 1);
+        return res;
+    }
+}
