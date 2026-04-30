c1cb220eb7989cb663fc034e11acf29f342d6144
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-04-30 17:35:09 +0600

    Add: products-of-array-discluding-self - submission-27

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-27.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    productExceptSelf(nums) {
+        let res = [];
+        for(let i = 0; i < nums.length; i++){
+            let prod = 1;
+            for(let j = 0; j < nums.length; j++){
+                if(i !== j){
+                    prod *= nums[j]
+                }
+            }
+            res.push(prod);
+        }
+        return res
+    }
+}
