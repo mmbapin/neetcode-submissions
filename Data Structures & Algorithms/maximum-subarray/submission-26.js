7b02c6dbec6b1504cde0c102f59cc49cfaf1a3ca
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-05-01 19:31:00 +0600

    Add: maximum-subarray - submission-26

---
+++ Data Structures & Algorithms/maximum-subarray/submission-26.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums) {
+        let dp = [...nums];
+        for(let i = 1; i < nums.length; i++){
+            dp[i] = Math.max(nums[i], nums[i] + dp[i - 1])
+        }
+        return Math.max(...dp);
+    }
+}
