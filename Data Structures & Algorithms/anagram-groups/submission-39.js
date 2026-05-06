5e91d6becec01e852df5bc921cbda0c58ecb953c
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-05-06 21:00:46 +0600

    Add: anagram-groups - submission-39

---
+++ Data Structures & Algorithms/anagram-groups/submission-39.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @return {string[][]}
+     */
+    groupAnagrams(strs) {
+        let count = {};
+        for(let s of strs){
+            let newStr = s.split("").sort().join("");
+            if(!count[newStr]){
+                count[newStr] = []
+            }
+            count[newStr].push(s);
+        }
+        return Object.values(count);
+    }
+}
