325a06bcd19499ab8c782ac1ba5f7da9c691776b
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-05-15 12:20:46 +0600

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.js
@@ -0,0 +1,33 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {number}
+     */
+    minMeetingRooms(intervals) {
+        let time = [];
+        for(let i of intervals){
+            time.push([i.start, 1]);
+            time.push([i.end, -1])
+        }
+
+        time.sort((a, b) => (a[0] === b[0] ? a[1] - b[1] : a[0] - b[0]));
+        let res = 0;
+        let count = 0;
+
+        for(let t of time){
+            count += t[1];
+            res = Math.max(res, count);
+        }
+        return res;
+    }
+}
