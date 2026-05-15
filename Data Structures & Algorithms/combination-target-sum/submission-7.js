dd4f277284961f2051e0a2ccb5cde31ab4ce258c
M.M.BAPIN <31760382+mmbapin@users.noreply.github.com>
2026-05-15 11:14:21 +0600

    Add: combination-target-sum - submission-7

---
+++ Data Structures & Algorithms/combination-target-sum/submission-7.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @returns {number[][]}
+     */
+    combinationSum(nums, target) {
+        let res = [];
+
+        const makeCombination = (idx, comb, total) => {
+            if(total === target){
+                res.push([...comb]);
+                return;
+            }
+            if(total > target || idx >= nums.length){
+                return;
+            }
+
+            comb.push(nums[idx]);
+            makeCombination(idx, comb, total + nums[idx]);
+            comb.pop();
+            makeCombination(idx + 1, comb, total);
+        }
+        makeCombination(0, [], 0);
+        return res;
+    }
+}
